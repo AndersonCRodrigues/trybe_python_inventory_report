aa458a5a01daeb32bca055e00b0740b49de400fa
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-07-12 16:28:33 -0300

    req: 02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,26 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        "123",
+        "Test Product",
+        "Test Company",
+        "2023-01-01",
+        "2023-12-31",
+        "ABC123",
+        "Store in a cool and dry place",
+    )
+    expected_string = (
+        "The product 123 - Test Product "
+        "with serial number ABC123 "
+        "manufactured on 2023-01-01 "
+        "by the company Test Company "
+        "valid until 2023-12-31 "
+        "must be stored according to the following instructions: "
+        "Store in a cool and dry place."
+    )
+
+    actual_string = str(product)
+
+    assert actual_string == expected_string
